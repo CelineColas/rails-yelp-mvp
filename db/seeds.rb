d3ab3371e87b32419617a3d929958ef9fc20b929
Celine Colas <colasceline88@gmail.com>
2016-08-10 20:33:52 +0200

    My app done without design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "La Belle Equipe", address: "Brussels", category: "french", phone_number: "088799656")
+Restaurant.create(name: "Le Shangai", address: "Paris", category: "chinese", phone_number: "757937798")
+Restaurant.create(name: "A la bonne frite", address: "Liège", category: "belgian", phone_number: "075369842")
+Restaurant.create(name: "Au beau gaufrier", address: "Namur", category: "belgian", phone_number: "324543078")
+Restaurant.create(name: "Pizzera dela mamma", address: "Rome", category: "italian", phone_number: "754389875")
+Restaurant.create(name: "Wok garden", address: "Waremme", category: "japanese", phone_number: "674930631")
+Restaurant.create(name: "Le Hokusai", address: "Tokyo", category: "japanese", phone_number: "697437521")
+Restaurant.create(name: "Da Luigi", address: "Sienne", category: "italian", phone_number: "976432765")
